7c5e3b8767b2afc064f21631b4adc6fadb85c9de
vipinarjun144 <vipin.arjun144@gmail.com>
2022-06-09 12:31:41 +0530

    First Commite with Properties

---
+++ ecomerse/src/main/java/in/nareshIT/vipin/EcomerseApplication.java
@@ -0,0 +1,13 @@
+package in.nareshIT.vipin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerseApplication.class, args);
+	}
+
+}
---
+++ ecomerse/src/test/java/in/nareshIT/vipin/EcomerseApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nareshIT.vipin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
